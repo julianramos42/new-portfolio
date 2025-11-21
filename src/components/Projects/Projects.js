00ca574f2de8fb0e8147952b6b0a860f04fdc785
Julián Ramos <julian26.ramos@gmail.com>
2025-11-21 16:27:45 -0300

    se agrega un proyecto mas

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import data from '../../data.json'
+import Troncone from '../../Assets/Projects/troncone.png'
 import FranPortfolio from '../../Assets/Projects/franPortfolio.webp'
 import TuWebHoy from '../../Assets/Projects/tuwebhoy.webp'
 import RedSocial from '../../Assets/Projects/red-social.webp'
@@ -11,7 +12,7 @@ import Minga from '../../Assets/Projects/minga.webp'
 import PetShop from '../../Assets/Projects/petshop.webp'
 import ToDoApp from '../../Assets/Projects/todo-app.webp'
 
-const images = [FranPortfolio, TuWebHoy, RedSocial, Lance, Minga, PetShop, ToDoApp]
+const images = [Troncone, FranPortfolio, TuWebHoy, RedSocial, Lance, Minga, PetShop, ToDoApp]
 
 function Projects() {
   return (
